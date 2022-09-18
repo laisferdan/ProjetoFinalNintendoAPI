623949f7ab8bd7d9311730000b4d936de26d5cb1
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-09-17 23:07:31 -0300

    Update of .gitignore to commit Logs

---
+++ Logs/CustomLogs.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace ProjetoFinalNintendoAPI.Logs
+{
+    public class CustomLogs
+    {
+        const string PUT = "put";
+        const string PATCH = "patch";
+        const string DELETE = "delete";
+
+        public static void SaveLog(int id, string message, string title, string method, object? entityBefore = null, object entityAfter = null)
+        {
+            var now = DateTime.Now.ToString("G");
+
+            if (method.Equals(PUT, StringComparison.InvariantCultureIgnoreCase) || method.Equals(PATCH, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine($"{now} - {message} {id} - {title} - Alterado de {JsonSerializer.Serialize(entityBefore)} para {JsonSerializer.Serialize(entityAfter)}");
+            }
+            else if (method.Equals(DELETE, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine($"{now} - {message} {id} - {title} - Removido");
+            }
+        }
+    }
+}
