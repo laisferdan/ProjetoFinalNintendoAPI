06dc887243249ac285732c0fb35076a54e7c081a
Laís Fernandes Daniel <laisfd@hotmail.com>
2023-02-23 16:22:48 -0300

    fix: Change folder name to Authentication

---
+++ Authentication/TokenConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjetoFinalNintendoAPI.AuthorizationAndAuthentication
+{
+    public class TokenConfiguration
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public int ExpirationTimeInHours { get; set; }
+        public string Sub { get; set; }
+        public string Audience { get; set; }
+        public string Module { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+
+    }
+}
