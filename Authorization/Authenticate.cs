9c6814b74fe42a20e196d64062366223d9676d8e
Laís Fernandes Daniel <laisfd@hotmail.com>
2023-02-23 16:23:12 -0300

    fix: Change folder name to Authentication

--- Authorization/Authenticate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjetoFinalNintendoAPI.AuthorizationAndAuthentication
-{
-    public class Authenticate
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
